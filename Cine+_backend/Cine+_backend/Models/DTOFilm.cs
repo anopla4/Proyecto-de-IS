8f8e0dc06cabb8c8005634656ef31adca4733316
ams1927 <abel.molina@estudiantes.matcom.uh.cu>
2021-06-06 13:38:37 -0400

    addDTOFilm

---
+++ Cine+_backend/Cine+_backend/Models/DTOFilm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cine__backend.Models
+{
+    public class DTOFilm
+    {
+        public Film Film { get; set; }
+        public IList<Genre> Genres { get; set; }
+    }
+}
