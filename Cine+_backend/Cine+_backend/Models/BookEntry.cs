face3b137398263111fa09cf39aa77cb1904a305
jlopetegui98 <jlopetegui98@gmail.com>
2021-06-05 20:29:32 -0400

    enumPaymentMethod

--- Cine+_backend/Cine+_backend/Models/BookEntry.cs
+++ Cine+_backend/Cine+_backend/Models/BookEntry.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,23 @@ namespace Cine__backend.Models
 {
     public class BookEntry
     {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public double Income { get; set; }
+        [Required]
+        public double Expense { get; set; }
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public PaymentMethod PaymentMethod { get; set; }
+    }
+
+    public enum PaymentMethod
+    {
+        efectivo,
+        crédito
     }
 }
--- Cine+_backend/Cine+_backend/Models/Reservation.cs
+++ Cine+_backend/Cine+_backend/Models/Reservation.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 
 namespace Cine__backend.Models
 {
-    public class Reservation
+    public class Reservation:Item
     {
+        
     }
 }
