54011f0a537fd7e2d7e0823efdd2843424b6f427
ams1927 <abel.molina@estudiantes.matcom.uh.cu>
2021-06-08 09:42:01 -0400

    MinimalUpdattingInUserFilmRepoA

---
+++ Cine+_backend/Cine+_backend/Interfaces/IUserFilmRepository.cs
@@ -0,0 +1,17 @@
+﻿using Cine__backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cine__backend.Interfaces
+{
+    public interface IUserFilmRepository
+    {
+        public UserFilm GetUserFilm(Guid userId, Guid filmId);
+        public IList<UserFilm> GetUserFilms();
+        public UserFilm AddUserFilm(UserFilm userFilm);
+        public UserFilm UpdateUserFilm(UserFilm userFilm);
+        public void DeleteUserFilm(Guid userId, Guid filmId);
+    }
+}
---
+++ Cine+_backend/Cine+_backend/Repositories/UserFilmRepository.cs
@@ -0,0 +1,37 @@
+﻿using Cine__backend.Interfaces;
+using Cine__backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cine__backend.Repositories
+{
+    public class UserFilmRepository : IUserFilmRepository
+    {
+        public UserFilm AddUserFilm(UserFilm userFilm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteUserFilm(Guid userId, Guid filmId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserFilm GetUserFilm(Guid userId, Guid filmId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<UserFilm> GetUserFilms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserFilm UpdateUserFilm(UserFilm userFilm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
